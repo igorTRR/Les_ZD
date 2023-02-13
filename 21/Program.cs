9cc3a086cc0de66a2b446844345c6216a1cc6a63
Igor <tararuev65@gmail.com>
2023-02-13 10:15:47 +0600

    )21Facterial

---
+++ 21/Program.cs
@@ -0,0 +1,14 @@
+﻿double Factorial(int n)
+
+{
+    if( n==1 ) return 1;
+    else return n*Factorial(n-1);
+}
+for (int i=1; i<20; i++)
+{
+
+}
+Console.Write( "Введите число n: " );
+int n =int.Parse(Console.ReadLine()!);
+Console.WriteLine();
+Console.WriteLine($"{n}! = {Factorial(n)}");
